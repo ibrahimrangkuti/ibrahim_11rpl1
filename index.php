3e287ac4dd3c7289e2f52750c5cbc6fdee7c3c07
ibrahimrangkuti <rangkutyibrahim765@gmail.com>
2023-05-18 12:51:24 +0700

    menambahkan file style.css dan memindahkan semua style

--- index.php
+++ index.php
@@ -2,119 +2,41 @@
 <html>
 
 <head>
-    <style>
-        * {
-            box-sizing: border-box;
-        }
-
-        input[type=text],
-        select,
-        textarea {
-            width: 100%;
-            padding: 12px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            resize: vertical;
-        }
-
-        label {
-            padding: 12px 12px 12px 0;
-            display: inline-block;
-        }
-
-        input[type=submit] {
-            background-color: #04AA6D;
-            color: white;
-            padding: 12px 20px;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-            float: right;
-        }
-
-        input[type=submit]:hover {
-            background-color: #45a049;
-        }
-
-        .container {
-            border-radius: 5px;
-            background-color: #f2f2f2;
-            padding: 20px;
-        }
-
-        .col-25 {
-            float: left;
-            width: 25%;
-            margin-top: 6px;
-        }
-
-        .col-75 {
-            float: left;
-            width: 75%;
-            margin-top: 6px;
-        }
-
-        /* Clear floats after the columns */
-        .row::after {
-            content: "";
-            display: table;
-            clear: both;
-        }
-
-        /* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
-        @media screen and (max-width: 600px) {
-
-            .col-25,
-            .col-75,
-            input[type=submit] {
-                width: 100%;
-                margin-top: 0;
-            }
-        }
-    </style>
+    <title>Form Biodata Siswa</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
-    <h2>Responsive Form</h2>
-    <p>Resize the browser window to see the effect. When the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other.</p>
+    <h1>Form Biodata Siswa</h1>
 
     <div class="container">
         <form action="/action_page.php">
             <div class="row">
                 <div class="col-25">
-                    <label for="fname">First Name</label>
+                    <label for="nama">Nama</label>
                 </div>
                 <div class="col-75">
-                    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+                    <input type="text" id="nama" name="nama" placeholder="Masukkan nama anda.">
                 </div>
             </div>
             <div class="row">
                 <div class="col-25">
-                    <label for="lname">Last Name</label>
+                    <label for="kelas">Kelas</label>
                 </div>
                 <div class="col-75">
-                    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-25">
-                    <label for="country">Country</label>
-                </div>
-                <div class="col-75">
-                    <select id="country" name="country">
-                        <option value="australia">Australia</option>
-                        <option value="canada">Canada</option>
-                        <option value="usa">USA</option>
+                    <select id="kelas" name="kelas">
+                        <option value="11 RPL 1">11 RPL 1</option>
+                        <option value="11 RPL 2">11 RPL 2</option>
                     </select>
                 </div>
             </div>
             <div class="row">
                 <div class="col-25">
-                    <label for="subject">Subject</label>
+                    <label for="alamat">Alamat</label>
                 </div>
                 <div class="col-75">
-                    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+                    <textarea id="alamat" name="alamat" placeholder="Masukkan alamat anda" style="height:200px"></textarea>
                 </div>
             </div>
             <br>
